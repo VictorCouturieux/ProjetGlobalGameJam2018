2ec86020ae92234e11620c32372cc07545dcc983
Le6barbare <Le6barbare@gmail.com>
2018-01-27 17:09:10 +0100

    Added Editor test Button
    +réorganisation des dossiers

---
+++ ProjetGlobalGameJam2018/Assets/Scripts/GameManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+	public GameObject Transmission;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	public void SpawnRandom() {
+		Instantiate(Transmission, Vector3.zero, Quaternion.AngleAxis(Random.Range(0.0f, 360.0f), Vector3.forward)); //Object Socks = 
+	}
+	
+}
+
+[CustomEditor(typeof(GameManager))]
+public class GameManagerEditor : Editor {
+    public override void OnInspectorGUI() {
+        DrawDefaultInspector();
+
+        GameManager myScript = (GameManager)target;
+        if (GUILayout.Button("Instanciate !")) {
+            myScript.SpawnRandom();
+        }
+    }
+}
---
+++ ProjetGlobalGameJam2018/Assets/Scripts/animSatelite.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class animSatelite : MonoBehaviour
+{
+	private const float Min = 10f;
+	private const float Max = 100f;
+	
+	private float _rotationSpeed = 50;
+	
+	private Vector2 _centre;
+
+	private void Start() {
+		_centre = new Vector3(0, 0, 1);
+	}
+
+	
+	private void Update()
+	{
+
+		if (Input.GetKey("up"))
+		{
+			_rotationSpeed++;
+			if (_rotationSpeed > Max)
+			{
+				_rotationSpeed = Max;
+			}
+		}
+		if (Input.GetKey("down"))
+		{
+			_rotationSpeed--;
+			if (_rotationSpeed < Min)
+			{
+				_rotationSpeed = Min;
+			}
+		}
+		
+		transform.RotateAround(_centre, Vector3.back, _rotationSpeed * Time.deltaTime);
+		
+//		print(rotationSpeed);
+	}
+
+	
+}
